228582585519788bbd5d7a92059bdcc9531ce390
Ran Kirlian <rankirlian@gmail.com>
2022-09-11 00:21:23 +0200

    Update components.js to include all items to show on createTodoHtml

--- src/js/components.js
+++ src/js/components.js
@@ -4,9 +4,13 @@ const divTodoList = document.querySelector('.todo-list');
 
 export const createTodoHtml = (todo) => {
   const htmlTodo = `
-   <li class=${(todo.completado) ? 'completed' : ''} data-id="abc">
+   <li class=${todo.completed ? 'completed' : ''} data-id="${todo.id}">
       <div class="view">
-        <input class="toggle" type="checkbox" checked>
+        <input 
+          class = "toggle"
+          type = "checkbox"
+          ${todo.completed ? 'checked' : ''} 
+        >
         <label>${todo.task}</label>
         <button class="destroy"></button>
       </div>
@@ -17,7 +21,8 @@ export const createTodoHtml = (todo) => {
   const div = document.createElement('div');
   div.innerHTML = htmlTodo;
 
-  divTodoList.append(div);
+  divTodoList.append(div.firstElementChild);
+  // By doing this we avoid creating a new div per each task, just the li
 
   return div;
 };
