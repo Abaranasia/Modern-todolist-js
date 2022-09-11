e4028b173837475725beaa669a0b5a3792f61059
Ran Kirlian <rankirlian@gmail.com>
2022-09-11 22:28:29 +0200

    Update todo-list class to add methods for LocalStorage save and load

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -2,28 +2,42 @@
 /* eslint-disable import/prefer-default-export */
 export class TodoList {
   constructor() {
-    this.todos = [];
+    this.loadFromLocalStorage();
   }
 
   newTodo(todo) {
     this.todos.push(todo);
+    this.saveToLocalStorage();
   }
 
   deleteTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id != id);
     // just != because compares strings and number
+    this.saveToLocalStorage();
   }
 
   toggleTodo(id) {
     for (const todo of this.todos) {
-      if (todo.id == id) {
+      if (todo.id == id) { // Only == because we compare string and number
         todo.completed = !todo.completed;
         break;
       }
-    }
+    };
+    this.saveToLocalStorage();
   }
 
   deleteCompleted() {
     this.todos = this.todos.filter((todo) => !todo.completed);
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  loadFromLocalStorage() {
+    this.todos = (localStorage.getItem('todos'))
+      ? JSON.parse(localStorage.getItem('todos', this.todos))
+      : [];
   }
 }
