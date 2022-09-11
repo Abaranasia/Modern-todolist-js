556ed2cd955a64122817048266469605d352447c
Ran Kirlian <rankirlian@gmail.com>
2022-09-11 23:07:39 +0200

    Update todo class to add fromJson static method to parse objects to intances

--- src/classes/todo.class.js
+++ src/classes/todo.class.js
@@ -1,6 +1,18 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable linebreak-style */
 /* eslint-disable import/prefer-default-export */
 export class Todo {
+  static fromJson({ task, id, completed, created }) {
+    // this static method converts an object to a todo instance
+    const tempTodo = new Todo(task);
+
+    tempTodo.id = id;
+    tempTodo.completed = completed;
+    tempTodo.created = created;
+
+    return tempTodo;
+  }
+
   constructor(task) {
     this.task = task;
 
